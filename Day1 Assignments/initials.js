7b7e765e457057ded391eb6232c65559d14b706b
Shiny212 <shinybelsiyar@gmail.com>
2026-04-30 17:24:21 +0530

    Added Day1 Assignments JS tasks

---
+++ Day1 Assignments/initials.js
@@ -0,0 +1,5 @@
+let fullName = "John Doe";
+
+let initials = fullName.split(" ")[0][0] + fullName.split(" ")[1][0];
+
+console.log(initials);
---
+++ Day1 Assignments/valueFinder.js
@@ -0,0 +1,4 @@
+let numbers = [10, 20, 30, 40, 50];
+
+console.log(numbers.includes(30));
+console.log(numbers.indexOf(50));
