e95a8e4d79078d86715b5c060972a51373b5f7be
Shiny212 <shinybelsiyar@gmail.com>
2026-05-08 12:06:21 +0530

    Add hoisting example Day 7

---
+++ Day 7/practice_prblm/hoisting.js
@@ -0,0 +1,20 @@
+// console.log(greet);
+
+// let greet = "Hello, World!";
+
+// console.log(greet);
+
+// var greet;
+// greet = "Hello, World!";
+
+// console.log(greet);
+
+// sayHi();
+
+// function sayHi() {
+//     console.log("Hello, World!");
+// }
+
+// var sayHi = () => {
+//     console.log("Hello");
+// };
