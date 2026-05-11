4d9ecbfec6dc18275b958b939fb9746fee0568c2
Shiny212 <shinybelsiyar@gmail.com>
2026-05-11 12:42:16 +0530

    Update Day 9 code files

--- Day 9/index.ts
+++ Day 9/index.ts
@@ -1,2 +1,13 @@
-let name = "Shiny";
-console.log(name);
+//let name = "Shiny";
+//console.log(name);
+let name: string = "Sindhu";
+let result: number | string;
+result = 45;
+result = "success";
+console.log(name, result);
+ 
+function add(a: number, b: number): string {
+    return "Sum: " + (a + b);
+}
+ 
+console.log(add(2, 20));
